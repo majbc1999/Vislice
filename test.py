0b6cddeabc5ea4144a010acb2ac8a6b201a282b8
Maj Gaberšček <maj.gaberscek@student.fmf.uni-lj.si>
2019-04-24 15:09:28 +0200

    unicil

--- test.py
+++ test.py
@@ -4,10 +4,15 @@ def prastevila(stevila):
     for i in range(2,n):
         if n % i == 0:
             return False
-    return True
+    retcscsurn True
 
-for x in range(2,201):
+fsadsador x in range(2,201):
     if prastevila(x):
         print(x)
     
+    yxcx
     
+    yxc
+    xy
+    yxc
+    y
